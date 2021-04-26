1d1988b2f27c936c8645fcdfe448d0c8d9ebde9b
Arthur Della Faille <arthur16.dellafaille@gmail.com>
2021-04-26 18:57:05 +0200

    modifications basket

--- view/basket/basket.js
+++ view/basket/basket.js
@@ -5,18 +5,31 @@ var basket = getBasket();
         for (let product of basket) {
             let furniture = new Furniture(product);
                 document.querySelector("#basket_content").innerHTML += `<table class="table">
-                                                                            <thead>
+                                                                            <thead class="thead-dark">
                                                                                 <tr><th>Article</th><th>Prix unitaire</th><th>Quantité</th><th>Prix total</th></tr>
-                                                                                <tr><th>${furniture.name}</th>
-                                                                                <th>${furniture.getFormatedPrice()}</th>
-                                                                                <th>${furniture.quantity}
-                                                                                    <button type="button" class="btn btn-primary" id="add">+</button>
-                                                                                    <button type="button" class="btn btn-primary" id="remove">-</button>
-                                                                                </th>
-                                                                                <th>
-                                                                                </tr>
                                                                             </thead>
-                                                                            <tbody id="cart-tablebody"></tbody>
-                                                                        </table>
-                                                                        <p>Sous total : <span class="subtotal"></span>€</p>`
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                <th scope="col">${furniture.name}</th>
+                                                                                <th scope="col">${furniture.getFormatedPrice()}</th>
+                                                                                <th scope="col">${furniture.quantity}
+                                                                                <span class="btn-group">
+                                                                                    <button type="button" class="btn btn-primary ml-3" id="add">+</button>
+                                                                                    <button type="button" class="btn btn-primary" id="remove">-</button>
+                                                                                </span>
+                                                                                </th>
+                                                                                <th scope="col">Prix total</th>
+                                                                                </tr>
+                                                                                <tr class="table-secondary">
+                                                                                <th scope="row" colspan="3">Total</th>
+                                                                                <td>Prix</td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>`;
+                                                                        document.querySelector("#add").addEventListener("click", function() {
+                                                                            addBasket(furniture);
+                                                                    });
+                                                                    document.querySelector("#remove").addEventListener("click", function() {
+                                                                            
+                                                                    });
         };
